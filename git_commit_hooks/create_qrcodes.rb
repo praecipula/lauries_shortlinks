cd5285e79b1bcc46dcb69ea9ab1f1a67b446b18e
Matt Bramlage <mbramlage@gmail.com>
2022-11-20 10:50:42 -0800

    Maybe removing tool-based QR code for React QR code

--- git_commit_hooks/create_qrcodes.rb
+++ git_commit_hooks/create_qrcodes.rb
@@ -5,6 +5,8 @@ qrcode = RQRCode::QRCode.new("https://praecipula.github.io/l/example")
 
 # NOTE: showing with default options specified explicitly
 svg = qrcode.as_svg(
+  size: 1,
+  level: :l,
   color: "000",
   shape_rendering: "crispEdges",
   module_size: 11,
