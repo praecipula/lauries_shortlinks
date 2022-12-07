91ce807ad20927caa9a9e5a25095753192e13654
Matt Bramlage <mbramlage@gmail.com>
2022-12-06 20:34:57 -0800

    Reading in the front matter and filtering down to offline_cache:true all works

--- runners/daily/010_offline_cache_pages.py
+++ runners/daily/010_offline_cache_pages.py
@@ -2,15 +2,15 @@
 
 import sys
 sys.path.append('./runners/lib')
+import glom
 import persist
 from tinydb import TinyDB, Query
 
 
 Pst = persist.Persist("offline_page_cache")
 
-frontmatter_db_file = "./runners/data/read_page_frontmatter.json"
-
 # Open in read-only mode and find all frontmatter data that has an offline_cache value set to True
+frontmatter_db_file = "./runners/data/read_page_frontmatter.json"
 fmdb = TinyDB(frontmatter_db_file, access_mode='r')
 
 # Am I really getting any value out of TinyDB here? I'm just getting the last record's data field...
@@ -22,15 +22,17 @@ def front_matter_filter(page):
     # This returns false for not existing in that path and
     # for value not matching.
 
-    #TODO: do this with glom.
-    return page.get('front_matter_data') and \
-        page['front_matter_data'].get('wait') and \
-        page['front_matter_data']['wait'] == 5
+    try:
+        offline_cache = glom.glom(page, 'front_matter_data.offline_cache')
+    except glom.core.PathAccessError as e:
+        return False
+    return offline_cache
+
 
 matching_front_matter_pages = filter(front_matter_filter, last_run_data)
-print(len(last_run_data))
-print(len(list(matching_front_matter_pages)))
+print(list(matching_front_matter_pages))
 
+# TODO: get the page with Selenium
 
 #Pst.add_db_run(file_data)
 #Pst.truncate_db_runs()
