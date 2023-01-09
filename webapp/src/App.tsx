2284bf49e58ee68568156d306ed2f17c3425b91f
Matt Bramlage <mbramlage@gmail.com>
2023-01-08 19:52:13 -0500

    Tied together React components. I'm not sure that the setState propagation is right, but it seems to work ish.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,22 +3,73 @@ import './App.css';
 import { QRCode } from 'react-qrcode-logo';
 
 
-/* Accepting any props is actually what we want.
- * These are a big ol' bag of props and they might be or mightn't be relevant
- * to any particular component we build.
- * In other words, App is flexible and can, for instance, render different
- * subcomponents depending on the props.
- */
-class App extends React.Component<any> {
+type iQRCCProps = {
+  seedContent: string,
+  contentUpdate: Function
+}
+
+type iQRCCState = {
+  content: string;
+  renderStatic: boolean
+}
+
+class QRCodeContent extends React.Component<iQRCCProps, iQRCCState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      content: props.seedContent,
+      renderStatic: true
+    }
+  }
+
+  inputChanged(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log("Input changed " + event.currentTarget.value);
+    this.setState({content: event.currentTarget.value});
+    this.props.contentUpdate(event.currentTarget.value);
+  }
+
+  render() {
+    var codeText = <p>this.props.seedContent</p>
+    if (this.state.renderStatic) {
+      codeText = <p id="codeText" style={{width: "100%", textAlign: "center", margin: "0", fontWeight: "700", overflowWrap:"break-word"}}>{this.state.content}</p>
+    } else {
+      codeText = <input type="text" id="codeText" name="codeText" value={this.state.content} onChange={event=>this.inputChanged(event)}></input>
+    }
+    return codeText
+  }
+}
+
+type iAppProps = {
+  codeContent?: string,
+  iconUrl?: string,
+  errorCorrection?: "H" | "M" | "L" | "Q";
+}
+
+type iAppState = {
+  qrContent: string
+}
+
+class App extends React.Component<iAppProps, iAppState> {
+
+
+  constructor(props: any) {
+    super(props);
+    var potentialQrContent = document.location.href;
+    if (this.props.codeContent) {
+      potentialQrContent = this.props.codeContent;
+    }
+    this.state = {
+      qrContent: potentialQrContent
+    };
+  }
+
+  qrContentUpdated(newContent: string) {
+    this.setState({qrContent: newContent});
+  }
 
   render() {
     console.log(this.props);
-    var qrText = ""
-    if (this.props.codeContent != null) {
-      qrText = this.props.codeContent
-    } else {
-      qrText = document.location.href
-    }
     type optionalComponentProps = {
       logoImage?: string
       ecLevel?: "H" | "M" | "L" | "Q";
@@ -33,8 +84,10 @@ class App extends React.Component<any> {
       errorCorrection = this.props.errorCorrection;
     }
     console.log(optionalProps)
+    
+
     var component = <div style={{ height: "400px", width: '320px' }}>
-                  <QRCode value={qrText}
+                  <QRCode value={this.state.qrContent}
                     size={300}
                     qrStyle="dots"
                     fgColor="#262664"
@@ -58,7 +111,7 @@ class App extends React.Component<any> {
                     ecLevel={errorCorrection}
                       {...optionalProps}
                       />
-                  <p style={{width: "100%", textAlign: "center", margin: "0", fontWeight: "700", overflowWrap:"break-word"}}>{qrText}</p>
+                    <QRCodeContent seedContent={this.state.qrContent} contentUpdate={(newContent: string) => this.qrContentUpdated(newContent)} />
                 </div>
     return (
       <div>
