c0871b83d626d01c11e6f2db2a90ac10d27061a5
Matt Bramlage <mbramlage@gmail.com>
2023-03-23 11:05:21 -0700

    Added credential fetching from env variable. Now to make it easier to ensure that this environment variable is set.

--- runners/lib/google_cloud_storage.py
+++ runners/lib/google_cloud_storage.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 import logging
+import os
 
 BUCKETNAME="matt_dot_directory_storage"
 
+from google.oauth2 import service_account
 from google.cloud import storage
+import json
 
-client = storage.Client().from_service_account_json("/Users/matt/Downloads/future-medley-249022-ec5906f38fd6.json")
+
+CREDENTIALS=json.loads(os.environ['CREDENTIALS'])
+creds = service_account.Credentials.from_service_account_info(CREDENTIALS)
+client = storage.Client(project=CREDENTIALS["project_id"], credentials=creds)
 
 bucket = client.bucket(BUCKETNAME)
 
@@ -17,5 +23,4 @@ testfilepath = "./" + testfilename
 with open(testfilepath, 'rb') as stream:
     blob = bucket.blob(testfilename)
     blob.upload_from_string(stream.read(), testfilename)
-    blob.make_public()
     print(blob.public_url)
--- runners/lib/testfile.md
+++ runners/lib/testfile.md
@@ -2,3 +2,5 @@ This is simply a test file
 ===
 
 I'm using this to test Google Cloud Storage access.
+
+And this line is modified; let's check re-upload semantics.
